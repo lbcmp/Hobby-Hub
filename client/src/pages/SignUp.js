2db88b33b54cdccf43d6d472f925e6251bbd5285
ibnat136 <ibnatnuha42@gmail.com>
2021-12-12 13:46:58 -0500

    updated signup

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './signup.css';
 import { AuthContext } from '../context/AuthContext';
 import { Redirect } from 'react-router-dom';
+
 class SignUp extends React.Component{
     state={
         email:'',
@@ -36,6 +37,33 @@ class SignUp extends React.Component{
           .catch((err) => {
             this.setState({ failed: true });
           });
+
+
+          fetch("/api/users/", {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({content: this.state.content}),
+          })
+            .then(res => {
+              if(res.ok) {
+                return res.json()
+              }
+      
+              throw new Error('Content validation');
+            })
+            .then(post => {
+              this.setState({
+                success: true,
+              });
+            })
+            .catch(err => {
+              this.setState({
+                error: true,
+              });
+            });
     }
     render(){
         const { from } = this.props.location.state || { from: { pathname: '/' } };
@@ -54,22 +82,30 @@ class SignUp extends React.Component{
                 {err}
                 <div>
                     <form onSubmit={this.signup}>
-                        <p className="header-font">Hobbies Hubs</p>
+                        <p className="header-font">Hobbies Hub</p>
                         <input className='input' type='fname' name='fname' placeholder='First Name' required onChange={this.fieldChanged('fname')}/>
                         <input className='input' type='lname' name='lname' placeholder='Last Name' required onChange={this.fieldChanged('lname')}/>
                         <input className='input' type='email' name='email' placeholder='Email' required onChange={this.fieldChanged('email')}/>
                         <input className='input' type='uemail' name='username' placeholder='Username' required onChange={this.fieldChanged('username')}/>
                         <input className='input' type='password' name='password' placeholder='Password' required onChange={this.fieldChanged('password')}/>
+                        <input className='input' type='age' name='age' placeholder='Age' required onChange={this.fieldChanged('age')}/>
                         <p className="hobby-option">Choose a Hobby</p>
                         <div className="hobbies">
                         <button className='button' onSubmit={this.handleSubmit}>Art</button>
                         <button className='button' onSubmit={this.handleSubmit}>Interior Design</button>
                         </div>
-                        <div className="list">
-                        <input className='list' type='checkbox' name='checkbox' value='I have a bike'/>
-                        <p>I have a bike</p>
+                        <div className="choice">
+                        <input className="list" type='checkbox' name='checkbox' value='I am looking for a project partner'/>
+                        <label className="label">I am looking for a project partner 
+                        
+                        </label>
+                        <br/>
+                        <input className="list" type='checkbox' name='checkbox' value='I am looking for a project partner'/> 
+                        <label className="label"> I am looking for somoen to talk to
+                        
+                        </label>
                         </div>
-                        <button className='button' onSubmit={this.handleSubmit}>Sign Up</button>
+                        <button className='signup' onSubmit={this.handleSubmit}>Sign Up</button>
                     </form>
                 </div>
             </div>
